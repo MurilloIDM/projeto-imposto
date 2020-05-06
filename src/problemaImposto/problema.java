496ba7caad0dc946b7ed482f535a00a6f38166cf
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-06 14:16:05 -0300

    Leitura dos dados

--- src/problemaImposto/problema.java
+++ src/problemaImposto/problema.java
@@ -1,9 +1,30 @@
 package problemaImposto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class problema {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
+		
+		//Declaração e leitura de variáveis
+		double rendaAnualSalario, rendaAnualServicos, rendaAnualCapital, gastosMedicos, gastosEducacionais;
+		
+		System.out.print("Renda anual com salário: ");
+		rendaAnualSalario = input.nextDouble();
+		System.out.print("Renda anual com prestação de serviços: ");
+		rendaAnualServicos = input.nextDouble();
+		System.out.print("Renda anual com ganho de capital: ");
+		rendaAnualCapital = input.nextDouble();
+		System.out.print("Gastos médicos: ");
+		gastosMedicos = input.nextDouble();
+		System.out.print("Gastos educacionais: ");
+		gastosEducacionais = input.nextDouble();
+		
+		input.close();
 
 	}
 
