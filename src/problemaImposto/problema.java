6989dc41b213b24d7e23351ed74c32981f3fdf63
Murillo Isidoro de Medeiros <44484530+MurilloIDM@users.noreply.github.com>
2020-05-06 14:33:17 -0300

    Consolidado de renda

--- src/problemaImposto/problema.java
+++ src/problemaImposto/problema.java
@@ -12,6 +12,9 @@ public class problema {
 		
 		//Declaração e leitura de variáveis
 		double rendaAnualSalario, rendaAnualServicos, rendaAnualCapital, gastosMedicos, gastosEducacionais;
+		double salarioMensal, impostoSalario, impostoServicos, impostoGanhos;
+		
+		impostoSalario = 0;
 		
 		System.out.print("Renda anual com salário: ");
 		rendaAnualSalario = input.nextDouble();
@@ -23,6 +26,29 @@ public class problema {
 		gastosMedicos = input.nextDouble();
 		System.out.print("Gastos educacionais: ");
 		gastosEducacionais = input.nextDouble();
+		
+		//Consolidando rendas
+		System.out.println();
+		System.out.println("RELATÓRIO DE IMPOSTO DE RENDA:");
+		System.out.println();
+		
+		salarioMensal = rendaAnualSalario / 12.0;
+		
+		if (salarioMensal < 3000.00) {
+			impostoSalario = 0;
+		} else if (salarioMensal >= 3000 && salarioMensal <= 5000) {
+			impostoSalario = rendaAnualSalario * 0.10;
+		} else {
+			impostoSalario = rendaAnualSalario * 0.20;
+		}
+		
+		impostoServicos = rendaAnualServicos * 0.15;
+		impostoGanhos = rendaAnualCapital * 0.20;
+		
+		System.out.println("CONSOLIDANDO RENDA: ");
+		System.out.printf("Imposto sobre salário: %.2f%n", impostoSalario);
+		System.out.printf("Imposto sobre serviços: %.2f%n", impostoServicos);
+		System.out.printf("Imposto sobre ganho de capital: %.2f%n", impostoGanhos);
 		
 		input.close();
 
